955f8fece3f3845a96e3df35ebebc99b5c701a55
spriveen <rpeanuka@gmail.com>
2024-12-12 00:09:31 -0800

    update blogItems.jsx

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,3 +1,5 @@
+'use client'
+import BlogItem from "@/Components/BlogItem";
 import Header from "@/Components/Header";
 
 
@@ -5,6 +7,7 @@ export default function Home() {
   return (
    <>
    <Header />
+   <BlogItem />
    </>
   )
 }
