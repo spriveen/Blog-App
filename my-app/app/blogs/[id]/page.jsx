e4131e0e7a5eb5221a4d81393b65152d22e648c0
spriveen <rpeanuka@gmail.com>
2024-12-15 08:26:37 -0800

    update page.jsx

--- my-app/app/blogs/[id]/page.jsx
+++ my-app/app/blogs/[id]/page.jsx
@@ -2,6 +2,7 @@
 import { assets, blog_data } from '@/Assets/assets';
 import Footer from '@/Components/Footer';
 import Image from 'next/image';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 
 const page = ({params}) => {
@@ -26,7 +27,9 @@ const page = ({params}) => {
     return (data?<>
     <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'> 
     <div className='flex justify-between items-center'>
+      <Link href='/'>
       <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto'/>
+      </Link>
       <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000px]'>
         Get Started <Image src={assets.arrow} alt='' />
         </button>
@@ -51,7 +54,7 @@ const page = ({params}) => {
        <p className='my-3'>Before you can manage your lifestyle, yu must have a clear understanding of what you want to achive. Start by reflecting on your values, aspirations and long term-goals. </p>
        <p className='my-3'>Before you can manage your lifestyle, yu must have a clear understanding of what you want to achive. Start by reflecting on your values, aspirations and long term-goals. </p>
        <h3 className='my-5 text-[18px] font-semibold'>Conclusion</h3>
-       <p className='my-3'>Managing your life style is a journey that require commitent and self-awarness. By following this step by step Guide you can fake control of your life and make meaningful changes that lead to more balanced and fullfing life style.Remember that its okay seek support and guidness from professional or mentors along the way. Your well-being and happiness are worth the effort </p>
+       <p className='my-3'>Managing your life style is a journey that require commitent and self-awarness. By following this step by step Guide you can fake control of your life and make meaningful changes that lead to more balanced and fullfing life style.Remember that its okay seek support and guidness from professional or mentors along the way. Your well-being and happiness are worth the effort. </p>
        <div className='my-24'>
         <p className='text-black font font-semibold my-4'>Share this article on social media</p>
         <div className='flex'>
